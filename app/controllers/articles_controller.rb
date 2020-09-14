d30fd4d15d6c2647267ff6b5d7b3d6920a3d6f8a
kthong3 <kthongsakounh@gmail.com>
2020-09-14 12:01:11 -0500

    create article show pages and add name to user model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = current_user.email
+    @user = current_user
     @articles = Article.all
   end
 
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     @article.author = current_user
@@ -23,6 +27,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
