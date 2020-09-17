3ef69f438de0360649ec55e45e5117aa6d6ce614
kthong3 <kthongsakounh@gmail.com>
2020-09-17 11:36:18 -0500

    update readme and add model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Article do
+  let(:user) {User.create(name: "Ang", email: "ang@email.com", password: "password")}
+  
+  let(:article) {
+    Article.create(
+      author_id: user.id, 
+      title: "Avatar", 
+      text: "Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked."
+    )}
+
+  it 'has an author' do
+    expect(article.author).to be_an_instance_of User
+  end
+
+  it 'has a title' do
+    expect(article.title).to eq "Avatar"
+  end
+
+  it 'has a body of text' do
+    expect(article.text).to eq "Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked."
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:user1) {User.create(name: "Ang", email: "ang@email.com", password: "password", admin: true)}
+  let(:user2) {User.create(name: "Katara", email: "katara@email.com", password: "password")}
+
+  it 'has a name' do
+    expect(user1.name).to eq "Ang"
+  end
+
+  it 'confirms if user is an admin' do
+    expect(user1.admin).to eq true
+    expect(user2.admin).to eq false
+  end
 end
